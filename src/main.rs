861c59095171a81e5eb32449a553902e19eff4d6
TcePrepK <a.s.arda.simsek@gmail.com>
2025-12-03 19:44:41 +0300

    Initials of post service. Moved extensions to their own schema

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use config::Config;
+use sqlx::postgres::PgPoolOptions;
+
 mod db;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let service_name = "POST";
+
+    let config: Config = Config::init(service_name);
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(config.database_url.as_str())
+        .await?;
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    Ok(())
 }
